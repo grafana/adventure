b8572bdfe7bbbcf687b8fad6bd20ca3b50f87477
Github Action <GithubAction@nowhere.com>
2024-11-30 11:53:38 -0500

    auto player name assignment in demo script

--- demo.sh
+++ demo.sh
@@ -2,8 +2,17 @@
 
 export PLAYER="Demo User2"
 
+# Generate a new adventurer name!
+ENDPOINT=http://localhost:3001/api/adventurer
+response=$(curl --silent $ENDPOINT)
+
+player_name=$(echo $response | jq -r '.name')
+export PLAYER=$player_name
+
 ./interact.sh 'look around'
 ./interact.sh 'go to town'
 ./interact.sh 'blacksmith'
 ./interact.sh 'request sword'
 ./interact.sh 'heat forge'
+
+echo "Thus temporarily ends the adventures of $player_name"
